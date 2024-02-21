0ff2f5388cceb251273658170cffb6ad5c925a4f
Nils Kolvenbach <info@nils-kolvenbach.de>
2024-02-21 23:24:57 +0100

    Added type definition to dist and fixed exported file names

---
+++ .changeset/afraid-months-live.md
@@ -0,0 +1,5 @@
+---
+'@elek-io/ui': patch
+---
+
+Added type definition to dist and fixed exported file names
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import react from '@vitejs/plugin-react';
 import Path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: Path.resolve(__dirname, 'src/index.tsx'),
       name: 'UI',
-      fileName: 'ui',
+      fileName: 'index',
       formats: ['cjs', 'es'],
     },
     rollupOptions: {
